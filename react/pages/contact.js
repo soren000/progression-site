b91cf55f69b9a8a445c66ba2dc6f2fad8e8662e3
soren <me@soren.dev>
2019-08-25 16:00:59 -0700

    fixed mistake with layout children

--- react/pages/contact.js
+++ react/pages/contact.js
@@ -2,19 +2,20 @@ import Layout from '../components/Layout';
 
 const contact = props => (
     <>
-        <Layout />
-        <div>
-            <p>If you would like to contact me for any reason, please fill out the info below and I'll get back to you soon!</p>
+        <Layout>
             <div>
-                <form>
-                    <p>E-Mail (some place I can respond back to you)</p>
-                    <input type="text" />
-                    <p>Message</p>
-                    <textarea></textarea>
-                    <button type="submit">Send</button>
-                </form>
+                <p>If you would like to contact me for any reason, please fill out the info below and I'll get back to you soon!</p>
+                <div>
+                    <form>
+                        <p>E-Mail (some place I can respond back to you)</p>
+                        <input type="text" />
+                        <p>Message</p>
+                        <textarea></textarea>
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </Layout>
     </>
 );
 
